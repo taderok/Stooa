6f2462464b2ea5cc416c91fc720275655e86a123
GhassenDaoudi <ghassen.daoudi@esprit.tn>
2022-04-29 13:45:36 +0100

    up

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,11 +1,4 @@
-/*!
- * This file is part of the Stooa codebase.
- *
- * (c) 2020 - present Runroom SL
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 import React, { createContext, useContext, useReducer } from 'react';
 import { IConferenceStatus } from '@/jitsi/Status';
--- frontend/src/lib/jitsi-modules/Events.js
+++ frontend/src/lib/jitsi-modules/Events.js
@@ -1,11 +1,4 @@
-/*!
- * This file is part of the Stooa codebase.
- *
- * (c) 2020 - present Runroom SL
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 export const SEATS_CHANGE = 'seats:change';
 export const CONFERENCE_START = 'conference:start';
